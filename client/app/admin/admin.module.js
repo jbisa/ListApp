69c4b2958da5232b20230db90f62677ac4f3dabf
Jay Bisa <jaybisa@Jay-Bisas-MBP.hsd1.ma.comcast.net>
2015-12-26 12:31:38 -0500

    Set up MEAN stack.

---
+++ client/app/admin/admin.module.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('listAppApp.admin', [
+  'listAppApp.auth',
+  'ui.router'
+]);
---
+++ server/config/environment/development.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/listapp-dev'
+  },
+
+  // Seed database on startup
+  seedDB: true
+
+};
